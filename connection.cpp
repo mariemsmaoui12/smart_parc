1fb179bfaeca3d86918a90d65b6b897740ccbfa1
jaouher12 <jaouher.jaziri@esprit.tn>
2021-10-21 15:45:02 +0100

    .cpp

--- connection.cpp
+++ connection.cpp
@@ -1,5 +1,5 @@
 #include "connection.h"
-
+//Test tutoriel git
 Connection::Connection()
 {
 
@@ -9,8 +9,8 @@ bool Connection::createconnect()
 {bool test=false;
 QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
 db.setDatabaseName("test-bd");
-db.setUserName("system");//inserer nom de l'utilisateur
-db.setPassword("TilltedAhri");//inserer mot de passe de cet utilisateur
+db.setUserName("jaziri");//inserer nom de l'utilisateur
+db.setPassword("jaziri");//inserer mot de passe de cet utilisateur
 
 if (db.open())
 test=true;
